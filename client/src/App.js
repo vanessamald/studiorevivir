050c75b055304eb5d041c326de6e69f5f1d1828f
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-06-28 16:33:55 -0500

    adding About section and styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import About from './components/About';
 import Cursor from './components/cursor';
 import useTheme from './components/useTheme';
 
@@ -16,14 +17,13 @@ function App() {
     <div className={theme}>
     <Cursor/>
       <main className={theme}>
-        <Home/>
-        {/*
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
           </Routes>
         </BrowserRouter>
-      */}
+      
       </main>
     
     </div>
--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import useTheme from '../useTheme';
 import idark from '../../assets/images/i-dark.png';
 import ilight from '../../assets/images/i-light.png';
+import Navigation from '../Navigation';
 
 function About() {
     const [ theme ] = useTheme();
 
     return (
-        <div id='about'>
-            <h2 className='about-i-title'>Web Design & Development Focused on Brand</h2>
+        <div className='about-container'>
+            <Navigation/>
+            <h2 className='about-i-title'>Web Design & Development Focused on</h2>
             <div className='flex-row'>
                 <img alt='light or dark theme icon' className='about-i bkg-transparent' src={theme === 'dark' ? ilight : idark}></img>
                 <div className='about-i-container flex-column justify-center bkg-transparent'>
-                    <h2 className='about-i-content bkg-transparent' >mage</h2>
+                
+                    <h2 className='about-i-content bkg-transparent' >dentity</h2>
                     <h2 className='about-i-content bkg-transparent'>&</h2>
-                    <h2 className='about-i-content bkg-transparent'>dentity</h2>
+                    <h2 className='about-i-content bkg-transparent'>mage</h2>
+                   
+                    <h2 className='about-i-content bkg-transparent' >Brand</h2>
+                    
                 </div>
             </div>
         </div>
