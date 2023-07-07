d5887d7bf2192ffaf2f96c75000e93bf5059e808
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-07-07 18:29:26 -0500

    updating About component styling

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -10,14 +10,14 @@ function About() {
     return (
         <div className='about-container'>
             <Navigation/>
-            <h2 className='about-i-title'>Web Design & Development Focused on</h2>
-            <div className='flex-row'>
-                <img alt='light or dark theme icon' className='about-i bkg-transparent' src={theme === 'dark' ? ilight : idark}></img>
+            <h2 className='about-i-title flex-column justify-center text-animation' style={{animationDelay: '0.5s'}}>Web Design & Development Focused on Brand</h2>
+            <div className='flex-row justify-center'>
+                <img alt='light or dark theme icon' className='about-i bkg-transparent text-animation' src={theme === 'dark' ? ilight : idark}></img>
                 <div className='about-i-container flex-column justify-center bkg-transparent'>
-                    <h2 className='about-i-content bkg-transparent' >dentity</h2>
-                    <h2 className='about-i-content bkg-transparent'>&</h2>
-                    <h2 className='about-i-content bkg-transparent'>mage</h2>
-                    <h2 className='about-i-content bkg-transparent' >Brand</h2>
+                    <h2 className='about-i-content bkg-transparent text-animation' style={{animationDelay: '1s'}}>mage</h2>
+                    <h2 className='about-i-content bkg-transparent text-animation' style={{animationDelay: '1.5s'}}>&</h2>
+                    <h2 className='about-i-content bkg-transparent text-animation' style={{animationDelay: '2s'}}>dentity</h2>
+                    {/*<h2 className='about-i-content bkg-transparent text-animation' style={{animationDelay: '2.5s'}} >Brand</h2>*/}
                 </div>
             </div>
         </div>
