4977bcfd18f2303c645824ce7e857446707cac18
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-07-12 15:35:52 -0500

    updating registration form to include error messages

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,43 +1,50 @@
 import React, { useState } from 'react';
 
 function Register() {
-
+    // email registration
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    // status and error messages
     const [ status, setStatus ] = useState('Subscribe to our Newsletter!');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const submitForm = async (e) => {
         e.preventDefault();
-        setStatus('Thank you for subscribing!');
+        // reset form
         e.target.reset();
 
+        // form info
         let registerEmail = {
             name: name,
             email: email,
-        } 
-        if ( registerEmail === '') {
-          // setResult({
-            //   message: 'Please fill out all fields';
-            //   console.log('ERROR')
-        
-        } else {
+            } 
 
-    let response = await fetch("/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify(registerEmail),
-    });
-    if (response) {
-        console.log('EMAIL ADDED')
-    }
+        // if user enters no email and/or name set error message
+        if ( !name || !email) {
+            setErrorMessage('Please fill out all fields!')
+        } else {
+        let response = await fetch("/register", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(registerEmail),
+        });
+        // if response ok set status 
+        if (response.ok) {
+        const data = await response.json();
+        setStatus('Thank you for subscribing!'); 
+        } else {
+            // set error message if subscription not successful
+            setErrorMessage( 'Error occurred during registration');
+        }
     }
 }
 
     return (
         <div className='register-container'>
             <h2 className='register-title'>{status}</h2>
+            <h2 className='register-title'>{errorMessage}</h2>
             <form onSubmit={submitForm} className='register-form'>
                 <input className='register-input' type='text' placeholder='Name'onChange={(e) => setName(e.target.value)}></input>
                 <input className='register-input' type='text' placeholder='email@example.com' onChange={(e) => setEmail(e.target.value)} ></input>
