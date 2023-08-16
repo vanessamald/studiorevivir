3d0d44d89f5277c2a3af61322b28a8ec638b87a9
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-08-15 20:18:43 -0500

    updating navigation styling

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -35,18 +35,23 @@ function Navigation() {
 
     return (
         <div>
-            <button className={button} onClick={handleClick}>
+            <button className={button} onClick={handleClick} aria-label="Menu">
                     <div className='nav-line1'></div>
                     <div className='nav-line2'></div>
                     <div className='nav-line3'></div>
             </button>
             <div className={menu}>
-                <div className='menu-content flex-column justify-center align-center'>
-                    <SplitText text='Menu'/>
-                    <a className='text-animation font-classic' style={{animationDelay: '0.5s'}} href='/'><SplitText2 text='Home' /></a>
-                    <a className='text-animation font-classic' style={{animationDelay: '1s'}} href='/about'><SplitText2 text='About' /></a>
-                    <a className='text-animation font-classic' style={{animationDelay: '1.5s'}} href='/work'><SplitText2 text='Work' /></a>
-                    <Contact/>
+                <div className='menu-content flex-column justify-center'>
+                    <div className='menu-content-text flex-row space-between align-center'>
+                        <SplitText text='Menu'/>
+                        <p className='menu-issue text-animation'><em className='menu-issue-em'>revívír</em>STUDIO /<em className='menu-issue-em'> No.001</em></p>
+                    </div>
+                    <div className='menu-links-container flex-column justify-center'>
+                        <a className='text-animation font-classic' style={{animationDelay: '0.5s'}} href='/'><SplitText2 text='Home' /></a>
+                        <a className='text-animation font-classic' style={{animationDelay: '1s'}} href='/about'><SplitText2 text='About' /></a>
+                        <a className='text-animation font-classic' style={{animationDelay: '1.5s'}} href='/work'><SplitText2 text='Work' /></a>
+                        <Contact/>
+                    </div>
                     <Register/>
                 </div>
                 <div>
