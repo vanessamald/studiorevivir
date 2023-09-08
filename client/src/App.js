c511b2678d0efbeec337d35f4b2ad58aee8833de
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-09-08 12:55:09 -0500

    styling the preloader

--- client/src/App.js
+++ client/src/App.js
@@ -16,28 +16,33 @@ import Preloader from './components/Preloader';
 function App() {
   const [ theme, componentMounted ] = useTheme();
   const [ loading, setLoading ] = useState(true);
+  
 
+  // set timeout for loading effect and pass as a prop to Preloader
   useEffect(()=> {
     const delay = setTimeout(()=> {
       setLoading(false);
       clearTimeout(delay);
-    }, 1000);
+    }, 3000);
 
     return ()=> clearTimeout(delay);
   }, []);
+  
 
   return (
     <div className={theme}>
     <Cursor/>
       <main className={theme}>
         <BrowserRouter>
-          {loading ? <Preloader /> : null}
+
+          {/*{loading ? <Preloader loading={loading}/> : null}*/}
+
           <Routes>
-                  <Route path='/' element={loading ? <Preloader/> : <Home/>}></Route>
-                  <Route path='/about' element={<About/>}></Route>
-                  <Route path='/work' element={<Work/>}></Route>
-                  <Route path='/inquire' element={<Contact/>}></Route>
-                  <Route path='/services' element={<Services/>}></Route>
+                  <Route path='/' element={loading ? <Preloader loading={loading}/> : <Home/>}></Route>
+                  <Route path='/about' element={loading ? <Preloader loading={loading}/> : <About/>}></Route>
+                  <Route path='/work' element={loading ? <Preloader loading={loading}/> : <Work/>}></Route>
+                  <Route path='/inquire' element={loading ? <Preloader loading={loading}/> : <Contact/>}></Route>
+                  <Route path='/services' element={loading ? <Preloader loading={loading}/> : <Services/>}></Route>
           </Routes>
         </BrowserRouter>
       </main>
--- client/src/components/Preloader/index.js
+++ client/src/components/Preloader/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-function Preloader() {
+function Preloader({ loading }) {
+    /*
     const [ loading, setLoading ] = useState(true);
 
     useEffect(()=> {
@@ -8,15 +9,15 @@ function Preloader() {
         const loader = setTimeout(() => {
             setLoading(false);
             clearTimeout(loader);
-        }, 1000);
+        }, 3000);
 
         // cleanup function 
         return () => clearTimeout(loader);
     }, [])
+    */
 
     // conditionally render component and remove from DOM if condition is false
-    return loading ? <div className='preloader'>Loading...</div> : null;
-    
+    return loading ? <div className='preloader-container'> <div className='preloader'></div></div> : null;
 }
 
 export default Preloader;
