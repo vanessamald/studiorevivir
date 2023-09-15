b559d0222dc4b32c150918374bed4c740fbc5bec
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-09-15 18:56:57 -0500

    updating about content and styling

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -33,31 +33,44 @@ function About() {
                     <SplitText text='About' fontColor='theme-text-color'/>
                 </div>
             </div>
-            <div className='about-content-container' ref={ref}>
+            <div className='about-content-container' >
                 <div>
                     <div className='about-subtitle-container'>
-                        <p className={`font-classic em-med ${inView ? 'text-animation' : ''}`}>Hi! My name is Vanessa Maldonado, a Freelance Web Developer and the creator of <em className='font-italic bolder em-med'>revívír</em><em className='font-classic uppercase em-small font-normal'>studio</em></p>
+                        <p className='font-classic large-font text-animation'>Vanessa Maldonado is a Freelance Web Developer and the creator of <em className='font-italic bolder'>revívír</em><em className='font-classic uppercase font-normal medium-font'>studio</em></p>
                     </div>
                     
-                    <div className='about-subtitle-container-alternate'>
-                        <p className={`font-classic em-med ${inView ? 'slide-in' : ''}`}> <em className='font-italic bolder em-med'>revívír</em> in Spanish means to come alive again, to reawaken. <em className='font-italic bolder em-med'>revívír</em><em className='font-classic uppercase em-small font-normal'>studio</em> is a homage to the rebirth of my creativity through web design & development.</p>
+                    <div className='about-subtitle-container' ref={ref}>
+                        <p className={`font-classic medium-font ${inView ? 'slide-in' : ''}`}> <em className='font-italic bolder'>revívír</em> in Spanish means to come alive again, to <em>reawaken</em>.</p>
+                        <div className='container'></div>
+
+                        {/*<p className={`font-classic medium-font ${inView ? 'slide-in-right' : ''}`}><em className='font-italic bolder'>revívír</em><em className='font-classic uppercase font-normal em-med'>studio</em> is a homage to the rebirth of my creativity through web design & development.</p>*/}
+
+                        <p className={`font-classic medium-font ${inView ? 'slide-in-right' : ''}`}><em className='font-italic bolder'>revívír</em><em className='font-classic uppercase font-normal em-med'>studio</em> is a web design and development studio focused on elevating the online presence of small businesses through minimalistic and responsive designs that <em>recapture</em> their brand <em>essence</em>.</p>
+                        
                     </div>
-                    
-                    
+
+                    {/* 
                     <p>Well-known for being open and collaborative, leading with compassion, and having an eye for design.</p>
 
-                    <p> I want to help clients elevate their online presence through minimalistic and responsive web applications that showcase their brand.</p> 
-                </div>  
+                    <p>"I want to help clients elevate their online presence through minimalistic and responsive web applications that showcase their brand."</p> 
+                    */}
+
+                </div> 
+                <div className='about-subtitle-container-alternate'>
+                    <h2 className='em-med'>Why <em className='font-italic bolder'>revívír</em>?</h2> 
+                    {/*<p className='font-classic em-med'> <em className='em-med'>"</em>I want to help clients <em className='em-med'>elevate</em> their online presence through <em className='em-med'>artistic</em> and <em className='em-med'>seamless</em> designs that <em className='em-med'>showcase</em> their brand.<em className='em-med'>"</em></p> */}
+                    <p className='font-classic em-med'> <em className='large-font'>"</em>I want to help clients fall in love with their brand <em className='em-med'>all over again.</em><em className='large-font'>"</em></p> 
+                </div>
+                {/* 
                 <div className='flex-column about-content'>
-                    <h2>Why <em className='font-italic bolder'>revívír</em>?</h2>
-                    <p> Enhanced user experience converts to increased engagement by:</p>
+                    <p> Enhanced user experience through online presence converts to increased engagement by:</p>
                     <ul className='list-none'>
                         <li>Improving SEO and search visibility</li>
                         <li>Enhancing your Brand Perception</li>
                         <li>Competitive Differentiation</li>
                     </ul> 
-                    <p className='subtitle subtitle-container large-font font-classic uppercase'>Fall in love with your brand all over again</p>
                 </div>
+                */}
             </div>
             <div>
                 <Register/>
